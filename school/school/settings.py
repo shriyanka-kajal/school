3723cfda33d30216014f88b8887a8e4624455992
rishabhjain2018 <rishabhjain2018@gmail.com>
2015-10-12 15:49:25 +0530

    settings test changes

--- school/school/settings.py
+++ school/school/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': 'sch123',
             'USER': 'sch123',
-            'PASSWORD': '',
+            'PASSWORD': 'sch123',
             'HOST': '127.0.0.1',
             'PORT': '',
         }
